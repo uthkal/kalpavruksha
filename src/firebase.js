6ef7e577e2897eb0daf5a03575c2a035acf34115
Uthkal <uthkal@example.com>
2026-05-03 20:09:09 +0530

    Fresh clean start for Kalpavruksha website

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqES8CXKRI4ft33Q7f5WIs9mRMVW4IQdU",
+  authDomain: "kalpavruksha-d88a4.firebaseapp.com",
+  projectId: "kalpavruksha-d88a4",
+  storageBucket: "kalpavruksha-d88a4.firebasestorage.app",
+  messagingSenderId: "174503113616",
+  appId: "1:174503113616:web:dd6e9578edd1a0fe51b915",
+  measurementId: "G-HV5E99E5FJ"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
